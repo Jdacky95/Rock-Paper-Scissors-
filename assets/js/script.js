a9289e499ddeeef9e1449ce2eaaac36ad93b88fb
Jdacky95 <jamesdack88@gmail.com>
2022-06-15 10:47:33 +0000

    edit mistake javascript writing and check first button click funnction

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,32 @@
 // Declaring Game and Rock, Paper, Scissors div areas 
 
 
-const game = document.querySelector(".game")
-const buttons = document.querySelector(".btn1")
-const rock = document.getElementById("rock")
-const paper = document.getElementById("paper")
-const scissors = document.getElementById("scissors")
+const game = document.querySelector(".game");
+const buttons = document.querySelector(".btn1");
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
 
 // Declaring elements picked: 
 
-const picked = document.querySelector(".picked")
-const youPicked = get.getElementById("you-picked")
-const compPicked = get.getElementById("comp-picked")
-const result = get.getElementById("result")
+const picked = document.querySelector(".picked");
+const youPicked = document.getElementById("you-picked");
+const compPicked = document.getElementById("comp-picked");
+const result = document.getElementById("result");
 
 // Declaring scores to be updated: 
 
-const scores = document.querySelector(".scores")
-const yourScore = document.getElementById("your-score")
-const computerScore = document.getElementById("computer-score")
+const scores = document.querySelector(".scores");
+const yourScore = document.getElementById("your-score");
+const computerScore = document.getElementById("computer-score");
 
 // Declaring Turns left: 
 
-const turns = get.getElementById("turns-left")
+const turns = document.getElementById("turns-left");
+
+game.addEventListener('click', function(){
+    console.log("hello");
+})
 
 
 
