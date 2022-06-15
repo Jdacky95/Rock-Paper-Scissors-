280321b0f0642a3553465a43c744a45f8de75e99
Jdacky95 <jamesdack88@gmail.com>
2022-06-15 10:32:48 +0000

    Declaring variables to be used in functions throughout

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,28 @@
-yourScore = document.getElementById("your-score")
+// Declaring Game and Rock, Paper, Scissors div areas 
 
-console.log(yourScore)
+
+const game = document.querySelector(".game")
+const buttons = document.querySelector(".btn1")
+const rock = document.getElementById("rock")
+const paper = document.getElementById("paper")
+const scissors = document.getElementById("scissors")
+
+// Declaring elements picked: 
+
+const picked = document.querySelector(".picked")
+const youPicked = get.getElementById("you-picked")
+const compPicked = get.getElementById("comp-picked")
+const result = get.getElementById("result")
+
+// Declaring scores to be updated: 
+
+const scores = document.querySelector(".scores")
+const yourScore = document.getElementById("your-score")
+const computerScore = document.getElementById("computer-score")
+
+// Declaring Turns left: 
+
+const turns = get.getElementById("turns-left")
+
+
+
